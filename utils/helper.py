0a57bd87f0e65e19ae994252c563fb54c380a2d7
Lilin Lao <lll9p.china@gmail.com>
2016-05-27 13:54:04 +0800

    add coded_helper

--- utils/helper.py
+++ utils/helper.py
@@ -8,3 +8,16 @@ def input_y_helper(base_arr):
         y = 'y' + ''.join([str(n) for n in arr])
         ydict[y] = input('Input {}:'.format(y))
     return ydict
+
+
+def coded_helper(model):
+    p = model.p
+    base_arr = model.base_arr
+    r = []
+    for l in base_arr:
+        t = [0] * p
+        avg = 1 / len(l)
+        for i in l:
+            t[i - 1] = avg
+        r.append(t)
+    return r
