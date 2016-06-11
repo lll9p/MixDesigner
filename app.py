9247b1b7d11654289c4f0d3e8bf8639120d54bbe
Lilin Lao <lll9p.china@gmail.com>
2016-06-11 20:23:02 +0800

    modify

--- app.py
+++ app.py
@@ -1,107 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import logging
+import tornado.ioloop
+import tornado.web
+
+
 logging.basicConfig(level=logging.INFO)
-import asyncio
-import os
-import json
-import time
-from aiohttp import web
-import functools
 
 
-def get(path):
-    '''
-    Method GET decorator
-    '''
-    def decorator(func):
-        @functools.wraps(func)
-        def wrapper(*args, **kw):
-            return func(*args, **kw)
-        wrapper.__method__ = 'GET'
-        wrapper.__route__ = path
-    return decorator
+class MainHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        self.write("Hello, world")
 
 
-def post(path):
-    '''
-    Method POST decorator
-    '''
-    def decorator(func):
-        @functools.wraps(func)
-        def wrapper(*args, **kw):
-            return func(*args, **kw)
-        wrapper.__method__ = 'POST'
-        wrapper.__route__ = path
-    return decorator
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
 
-class RequestHandler():
-    def __init__(self,app,func):
-        self._app=app
-        self._func=func
-    async def __call__(self,request):
-        kw = None
-        if self._has_var_kw_arg or self._has_named_kw_args or self._required_kw_args:
-            if request.method == 'POST':
-                if not request.content_type:
-                    return web.HTTPBadRequest('Missing Content-Type.')
-                ct = request.content_type.lower()
-                if ct.startswith('application/json'):
-                    params = await request.json()
-                    if not isinstance(params, dict):
-                        return web.HTTPBadRequest('JSON body must be object.')
-                    kw = params
-                elif ct.startswith('application/x-www-form-urlencoded') or ct.startswith('multipart/form-data'):
-                    params = await request.post()
-                    kw = dict(**params)
-                else:
-                    return web.HTTPBadRequest('Unsupported Content-Type: %s' % request.content_type)
-            if request.method == 'GET':
-                qs = request.query_string
-                if qs:
-                    kw = dict()
-                    for k, v in parse.parse_qs(qs, True).items():
-                        kw[k] = v[0]
-        if kw is None:
-            kw = dict(**request.match_info)
-        else:
-            if not self._has_var_kw_arg and self._named_kw_args:
-                # remove all unamed kw:
-                copy = dict()
-                for name in self._named_kw_args:
-                    if name in kw:
-                        copy[name] = kw[name]
-                kw = copy
-            # check named arg:
-            for k, v in request.match_info.items():
-                if k in kw:
-                    logging.warning('Duplicate arg name in named arg and kw args: %s' % k)
-                kw[k] = v
-        if self._has_request_arg:
-            kw['request'] = request
-        # check required kw:
-        if self._required_kw_args:
-            for name in self._required_kw_args:
-                if not name in kw:
-                    return web.HTTPBadRequest('Missing argument: %s' % name)
-        logging.info('call with args: %s' % str(kw))
-        try:
-            r = await self._func(**kw)
-            return r
-        except APIError as e:
-            return dict(error=e.error, data=e.data, message=e.message)
-
-def add_route(app,fn):
-    pass
-def index(request):
-    return web.Response(body=b'Hello World!')
-async def init(loop):
-    app = web.Application(loop=loop)
-    app.router.add_route('GET', '/', index)
-    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
-    logging.info('server started at http://127.0.0.1:9000...')
-    return srv
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(init(loop))
-    loop.run_forever()
+    app = make_app()
+    app.listen(8888)
+    logging.info('listen on {}'.format(8888))
+    tornado.ioloop.IOLoop.current().start()
