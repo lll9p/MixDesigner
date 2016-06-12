88bae206b3f2025ac7ff24b28bb574de5eaa0fd3
Lilin Lao <lll9p.china@gmail.com>
2016-06-12 10:48:46 +0800

    fix input y issue

--- utils/helper.py
+++ utils/helper.py
@@ -7,7 +7,7 @@ def input_y_helper(base_arr):
     ydict = {}
     for arr in base_arr:
         y = 'y' + ''.join([str(n) for n in arr])
-        ydict[y] = input('Input {}:'.format(y))
+        ydict[y] = int(input('Input {}:'.format(y)))
     return ydict
 
 
