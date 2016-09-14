66deb2eaea10ab66a33de6a120de0e3889912632
Lilin Lao <lll9p.china@gmail.com>
2016-09-14 22:15:43 +0800

    modify score

--- models/SimplexCentroid.py
+++ models/SimplexCentroid.py
@@ -91,7 +91,7 @@ class SimplexCentroid():
         return prediction
 
     def score(self, X, y):
-        self.predict(X) - y
+        return np.sum(np.abs(self.predict(X) - y))/len(y)
 
     def __str__(self):
         if not self._response_surface_coef:
